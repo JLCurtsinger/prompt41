3d5ac619fbdab8148036a48587fdd08a0ebde317
JLCurtsinger <98992595+JLCurtsinger@users.noreply.github.com>
2025-11-24 13:53:08 -0700

    trying to fix where Im taking damage immediately upon spawning

--- src/game/Enemies/enemyDamage.ts
+++ src/game/Enemies/enemyDamage.ts
@@ -3,13 +3,30 @@
 
 import { useGameState } from '../../state/gameState';
 
+const DAMAGE_DEBUG = true;
+
 /**
  * Apply damage to the player from an enemy source
  * @param amount - Amount of damage to apply
  * @param source - Name of the enemy type (e.g., "Crawler", "Shambler")
  */
 export function applyDamageToPlayer(amount: number, source: string) {
-  const { applyDamage } = useGameState.getState();
+  const state = useGameState.getState();
+  const { applyDamage, playerHealth } = state;
+  
+  // Try to get playerPosition from state if available (may not exist)
+  const playerPosition = (state as any).playerPosition;
+  
+  if (DAMAGE_DEBUG) {
+    console.warn('[PLAYER DAMAGE]', {
+      amount,
+      source,
+      time: performance.now(),
+      playerHealth,
+      playerPosition,
+    });
+  }
+  
   applyDamage(amount, source);
 }
 
